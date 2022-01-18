cfceb6512804d42f44955547f33e4989554b27c4
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-01-17 20:19:23 -0600

    framing out the skeleton for the userController. Here we go...

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,33 @@
+const { User, Thought } = require('../models');
+
+// /api/users
+
+module.exports = {
+    getUsers(req, res) {
+
+    },
+
+    getSingleUser(req, res) {
+
+    }, 
+
+    createUser(req,res) {
+
+    },
+
+    updateUser(req,res) {
+
+    },
+
+    deleteUser(req, res) {
+
+    },
+
+    addFriend(req,res) {
+
+    },
+
+    removeFriend(req,res) {
+
+    },
+};
